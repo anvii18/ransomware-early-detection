9b452f8f55e126aab563f6e4a0de9bade804f80a
Anvesha <anvesha18bly@gmail.com>
2026-01-12 23:47:23 +0530

    Fix typo in BASE_DIR definition

--- simulation/simulation/simulator.py
+++ simulation/simulation/simulator.py
@@ -4,7 +4,7 @@ import random
 import string
 from datetime import datetime
 
-BASE_DIR = os.path.dirname(_file_)  # simulation folder
+BASE_DIR = os.path.dirname(__file__)  # simulation folder
 TARGET_FOLDER = os.path.join(BASE_DIR, "demo_test_files")
 
 
